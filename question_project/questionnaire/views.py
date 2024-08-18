2cc961971b7fc1286d09390ba123762844ada69b
RHETT CHASE <rhettjchase@gmail.com>
2024-08-18 13:10:40 -0700

    creates POST view to process_questions

--- question_project/questionnaire/views.py
+++ question_project/questionnaire/views.py
@@ -1,13 +1,19 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from .models import Question, Rule
+import json
 
+@csrf_exempt
+@require_POST
 def process_question(request, question_id):
-    response = request.GET.get('response')
-    
-    if not response:
-        return JsonResponse({'error': 'Response is required'}, status=400)
-    
     try:
+        body = json.loads(request.body)
+        response = body.get('response')
+        
+        if not response:
+            return JsonResponse({'error': 'Response is required'}, status=400)
+        
         question = Question.objects.get(id=question_id)
         rules = Rule.objects.filter(question=question)
 
@@ -18,5 +24,8 @@ def process_question(request, question_id):
                 if rule.message:
                     return JsonResponse({'message': rule.message})
         return JsonResponse({'message': 'No valid rule found'})
+    
     except Question.DoesNotExist:
         return JsonResponse({'error': 'Question not found'}, status=404)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON'}, status=400)
